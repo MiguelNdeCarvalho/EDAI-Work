6b5865a96df10f91784a1b0823dc83c22c42a8e9
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-06-16 12:54:07 +0100

    main.c: Convert all the keys to strings

--- main.c
+++ main.c
@@ -1,56 +1,57 @@
 #include <stdlib.h>
 #include <string.h>
+#include <wchar.h>
 #include "hashsep.c"
 
 void defineT9()
 {
     HashTable T9 = InitializeTable(11);
     // key 2
-    Insert(L'a', 2, T9);
-    Insert(L'b', 2, T9);
-    Insert(L'c', 2, T9);
-    Insert(L'á', 2, T9);
-    Insert(L'à', 2, T9);
-    Insert(L'â', 2, T9);
-    Insert(L'ã', 2, T9);
-    Insert(L'ç', 2, T9);
+    Insert(L"a", 2, T9);
+    Insert(L"b", 2, T9);
+    Insert(L"c", 2, T9);
+    Insert(L"á", 2, T9);
+    Insert(L"à", 2, T9);
+    Insert(L"â", 2, T9);
+    Insert(L"ã", 2, T9);
+    Insert(L"ç", 2, T9);
     // key 3
-    Insert(L'd', 3, T9);
-    Insert(L'e', 3, T9);
-    Insert(L'f', 3, T9);
-    Insert(L'é', 3, T9);
-    Insert(L'ê', 3, T9);
+    Insert(L"d", 3, T9);
+    Insert(L"e", 3, T9);
+    Insert(L"f", 3, T9);
+    Insert(L"é", 3, T9);
+    Insert(L"ê", 3, T9);
     // key 4
-    Insert(L'g', 4, T9);
-    Insert(L'h', 4, T9);
-    Insert(L'i', 4, T9);
-    Insert(L'í', 4, T9);
+    Insert(L"g", 4, T9);
+    Insert(L"h", 4, T9);
+    Insert(L"i", 4, T9);
+    Insert(L"í", 4, T9);
     // key 5
-    Insert(L'j', 5, T9);
-    Insert(L'k', 5, T9);
-    Insert(L'l', 5, T9);
+    Insert(L"j", 5, T9);
+    Insert(L"k", 5, T9);
+    Insert(L"l", 5, T9);
     // key 6
-    Insert(L'm', 6, T9);
-    Insert(L'n', 6, T9);
-    Insert(L'o', 6, T9);
-    Insert(L'ó', 6, T9);
-    Insert(L'ô', 6, T9);
-    Insert(L'õ', 6, T9);
+    Insert(L"m", 6, T9);
+    Insert(L"n", 6, T9);
+    Insert(L"o", 6, T9);
+    Insert(L"ó", 6, T9);
+    Insert(L"ô", 6, T9);
+    Insert(L"õ", 6, T9);
     // key 7
-    Insert(L'p', 7, T9);
-    Insert(L'q', 7, T9);
-    Insert(L'r', 7, T9);
-    Insert(L's', 7, T9);
+    Insert(L"p", 7, T9);
+    Insert(L"q", 7, T9);
+    Insert(L"r", 7, T9);
+    Insert(L"s", 7, T9);
     // key 8
-    Insert(L't', 8, T9);
-    Insert(L'u', 8, T9);
-    Insert(L'v', 8, T9);
-    Insert(L'ú', 8, T9);
+    Insert(L"t", 8, T9);
+    Insert(L"u", 8, T9);
+    Insert(L"v", 8, T9);
+    Insert(L"ú", 8, T9);
     // key 9
-    Insert(L'w', 9, T9);
-    Insert(L'x', 9, T9);
-    Insert(L'y', 9, T9);
-    Insert(L'z', 9, T9);
+    Insert(L"w", 9, T9);
+    Insert(L"x", 9, T9);
+    Insert(L"y", 9, T9);
+    Insert(L"z", 9, T9);
 
     PrintHashTable(T9);
 }
